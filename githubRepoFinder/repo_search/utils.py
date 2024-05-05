1c3d89ee340ccf71575a247994056ffbe9f295e6
ANSIL RAHMAN <rahmanansil777@gmail.com>
2024-05-05 20:28:38 +0530

    comments added in utils and view funcitons

--- githubRepoFinder/repo_search/utils.py
+++ githubRepoFinder/repo_search/utils.py
@@ -2,25 +2,40 @@ import os
 from dotenv import load_dotenv
 import requests
 
+# Load environment variables from .env file
 load_dotenv()
 
+# Retrieve GitHub token from environment variables
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
+# Function to search repositories on GitHub using the provided query
 def search_repositories(query):
 
+    # GitHub API URL for searching repositories
     url = 'https://api.github.com/search/repositories'
+    
+    # Set authorization headers with GitHub token
     headers = {'Authorization': f'token {GITHUB_TOKEN}'} 
+    
+    # Define search parameters
     params = {'q': query,'per_page': 500}
     
     try:
+        # Make a GET request to GitHub API
         response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status() 
         
+        # Check for HTTP errors
+        response.raise_for_status()  
+        
+        # Convert response to JSON format
         data = response.json()
+        
+        # Get repositories from response data
         repositories = data.get('items', [])
         
         return repositories
 
     except requests.exceptions.RequestException as e:
+        # Log and handle request errors
         print("Error:", e)
         return []
--- githubRepoFinder/repo_search/views.py
+++ githubRepoFinder/repo_search/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 from .utils import search_repositories
 from django.core.paginator import Paginator
 
-
+# View function for rendering the home page
 def home(request):
-
+    # Render the home.html template
     return render(request, 'home.html')
 
+# View function for handling search requests
 def search(request):
+    # Get the search query from the request
     query = request.GET.get('q')
+    
+    # If there is a query
     if query:
+        # Search repositories using the query
         repositories = search_repositories(query)
+        
+        # Render the results.html template with the repositories and query
         return render(request, 'results.html', {'repositories': repositories, 'query': query})
     else:
+        # Render the search.html template if there is no query
         return render(request, 'search.html')
