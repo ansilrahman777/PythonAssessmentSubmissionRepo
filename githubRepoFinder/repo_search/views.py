ce8beb9076da8d7621b70bd680f57fb29534b6b6
ANSIL RAHMAN <rahmanansil777@gmail.com>
2024-05-06 17:32:57 +0530

    listing page frontend design and the pagination completed

--- githubRepoFinder/repo_search/views.py
+++ githubRepoFinder/repo_search/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .utils import search_repositories
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # View function for rendering the home page
 def home(request):
@@ -17,8 +17,21 @@ def search(request):
         # Search repositories using the query
         repositories = search_repositories(query)
         
-        # Render the results.html template with the repositories and query
+        # Pagination
+        paginator = Paginator(repositories, 10)  # Show 10 repositories per page
+        page = request.GET.get('page')
+        try:
+            repositories = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            repositories = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            repositories = paginator.page(paginator.num_pages)
+        
+        # Render the results.html template with the repositories, query, and pagination
         return render(request, 'results.html', {'repositories': repositories, 'query': query})
     else:
         # Render the home.html template if there is no query
         return render(request, 'home.html')
+
