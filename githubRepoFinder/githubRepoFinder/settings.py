ce3620ffb30d1966c959f40054f4d27d85cfe063
ANSIL RAHMAN <rahmanansil777@gmail.com>
2024-05-07 11:53:47 +0530

    added vercel file and req file for hosting

--- githubRepoFinder/githubRepoFinder/settings.py
+++ githubRepoFinder/githubRepoFinder/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_-rn)xpa!)(*_^mjt=e013i=upc132^t0%tuf&gr9-vo1n!9_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
